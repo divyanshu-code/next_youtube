f5229393e8b4c8e5d6c79ef4aa21d284c6c0cb2a
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2026-01-03 20:20:39 +0530

    03012026

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className='bg-blue-950 flex justify-between px-10 py-5'>
         <h2>MyHommies</h2>
 
-        <div className='flex items-center gap-15'>
+        <div className='flex items-center gap-15 '>
             <Link href="/">Home</Link>
             <Link href="/About">About</Link>
             <Link href="/Course">Course</Link>
